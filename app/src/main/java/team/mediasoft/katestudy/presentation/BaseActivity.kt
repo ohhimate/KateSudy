f8b4002789abf28a8ad1bc2cc61bc457a15c9de4
Oleg Nekrasov <ONekrasov@alfabank.ru>
2021-12-20 19:33:27 +0300

    refactor

---
+++ app/src/main/java/team/mediasoft/katestudy/presentation/BaseActivity.kt
@@ -0,0 +1,4 @@
+package team.mediasoft.katestudy.presentation
+
+class BaseActivity {
+}
--- app/src/main/java/team/mediasoft/katestudy/presentation/MainActivity.kt
+++ app/src/main/java/team/mediasoft/katestudy/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package team.mediasoft.katestudy
+package team.mediasoft.katestudy.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import team.mediasoft.katestudy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
